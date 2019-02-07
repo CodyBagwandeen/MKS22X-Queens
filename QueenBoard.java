f337ba0f741061a9ef63d4a5cbeb47014c83a7b5
CodyBagwandeen <cbagwandeen@stuy.edu>
2019-02-07 09:54:08 -0500

    added a toString

--- QueenBoard.java
+++ QueenBoard.java
@@ -5,5 +5,21 @@ public class QueenBoard{
 
   }
 
+  public String toString(){
+    String output = "";
+    for(int i = 0; i < board.length; i++ ){
+      for(int j = 0; j < board[i].length; j++){
+        if(board[j][i] == -1){
+          output += "Q ";
+        } else {
+          output += "_ ";
+        }
+        if( j == board[i].length - 1){
+          output += "\n";
+        }
+      }
+    }
+    return output;
 
+  }
 }
