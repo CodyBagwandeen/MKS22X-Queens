f68e9b9901a8f3b020f64abc40443a8ed4618917
CodyBagwandeen <cbagwandeen@stuy.edu>
2019-02-07 20:25:34 -0500

    added testing for and added the function remove queen

--- QueenBoard.java
+++ QueenBoard.java
@@ -6,10 +6,27 @@ public class QueenBoard{
     System.out.println("Q1 = new QueenBoard(5)\n");
     QueenBoard Q1 = new QueenBoard(5);
     System.out.println("testing toString on blank board");
-    System.out.println("Q1 \n");
+    System.out.println("Q1\n");
     System.out.println(Q1);
 
-    System.out.println("Q1 \n");
+    System.out.println("Testing adding a queen");
+    System.out.println("Q1.addQueen(3,1)");
+    Q1.addQueen(3,1);
+
+    System.out.println("Q1\n");
+    System.out.println(Q1);
+
+    System.out.println("Q1.addQueen(2,0)");
+    Q1.addQueen(2,0);
+
+    System.out.println("Q1\n");
+    System.out.println(Q1);
+
+    System.out.println("Testing removing a Queen");
+    System.out.println("Q1.removeQueen(2,0)");
+    Q1.removeQueen(2,0);
+
+    System.out.println("Q1\n");
     System.out.println(Q1);
   }
 
@@ -42,7 +59,21 @@ public class QueenBoard{
   }
 
   private boolean removeQueen(int r, int c){
-    // do something later
-    return true;
+    if(board[r][c] == -1){
+      board[r][c] = 0;
+      return true;
+    }
+    return false;
+
+  }
+
+  /**
+  *@return false when the board is not solveable and leaves the board filled with zeros;
+  *        true when the board is solveable, and leaves the board in a solved state
+  *@throws IllegalStateException when the board starts with any non-zero value
+  */
+  public boolean solve(){
+    return false;
+    
   }
 }
