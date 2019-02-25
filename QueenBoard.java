1a512d8c9d928a69227502dc0ce2d5e42e6abcd8
CodyBagwandeen <cbagwandeen@stuy.edu>
2019-02-25 11:29:58 -0500

    added Mr.K testing

--- QueenBoard.java
+++ QueenBoard.java
@@ -69,6 +69,8 @@ public class QueenBoard{
     System.out.println("Q3 debug\n");
     System.out.println(Q3.toStringdebug());
 
+    runTest(5);
+
 
 
 
@@ -222,4 +224,22 @@ public class QueenBoard{
     return sum;
   }
 
+  //testcase must be a valid index of your input/output array
+public static void runTest(int i){
+QueenBoard b;
+int[]tests =   {1,2,3,4,5,8};
+int[]answers = {1,0,0,2,10,92};
+if(i >= 0 && i < tests.length ){
+  int size = tests[i];
+  int correct = answers[i];
+  b = new QueenBoard(size);
+  int ans  = b.countSolutions();
+
+  if(correct==ans){
+    System.out.println("PASS board size: "+tests[i]+" "+ans);
+  }else{
+    System.out.println("FAIL board size: "+tests[i]+" "+ans+" vs "+correct);
+  }
+}
+}
 }
